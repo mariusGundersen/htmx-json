ad1e3107760359a55b194fbe9ed9eb8c8775fe93
Marius Gundersen <me@mariusgundersen.net>
2025-07-13 13:30:34 +0200

    Build esm and cjs versions

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -1,8 +1,10 @@
-import { copyFile, mkdir, readFile, writeFile } from 'node:fs/promises';
-import uglify from 'uglify-js';
+import { copyFile, mkdir, readFile, writeFile } from "node:fs/promises";
+import uglify from "uglify-js";
 
-await mkdir('./dist', { recursive: true });
-await copyFile('./src/htmx-json.js', './dist/htmx-json.js');
-const file = await readFile('./src/htmx-json.js', 'utf-8');
+await mkdir("./dist", { recursive: true });
+await copyFile("./src/htmx-json.js", "./dist/htmx-json.js");
+const file = await readFile("./src/htmx-json.js", "utf-8");
 const { code } = uglify.minify(file);
-await writeFile('./dist/htmx-json.min.js', code);
+await writeFile("./dist/htmx-json.min.js", code);
+await writeFile("./dist/htmx-json.mjs", `${file}\nexport default htmxJson;`);
+await writeFile("./dist/htmx-json.cjs", `${file}\module.exports htmxJson;`);
