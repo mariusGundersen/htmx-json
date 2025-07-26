1f0093505fd5a0d2efe4bfe03e3475067af4fb6a
Marius Gundersen <me@mariusgundersen.net>
2025-07-26 09:07:46 +0200

    chore: Added basic benchmarking

---
+++ benchmark/text.benchmark.js
@@ -0,0 +1,26 @@
+suite("setting text", () => {
+  benchmark(
+    "native",
+    () => {
+      this.div.textContent = `random ${Math.random()} value`;
+    },
+    {
+      setup() {
+        this.div = document.createElement("div");
+      },
+    }
+  );
+
+  benchmark(
+    "swap",
+    () => {
+      htmxJson.swap(this.div, {});
+    },
+    {
+      setup() {
+        this.div = document.createElement("div");
+        this.div.innerHTML = `<span .text-content="\`random \${Math.random()} value\`"></span>`;
+      },
+    }
+  );
+});
