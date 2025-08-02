81c7a9046bc107fd8e953f55a2972fd6b6caa502
Marius Gundersen <me@mariusgundersen.net>
2025-08-02 08:53:26 +0200

    Generate sourcemaps, fixing #31

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -4,13 +4,18 @@ import uglify from "uglify-js";
 await mkdir("./dist", { recursive: true });
 await copyFile("./src/htmx-json.js", "./dist/htmx-json.js");
 const file = await readFile("./src/htmx-json.js", "utf-8");
-const { code } = uglify.minify(file, {
+const { code, map } = uglify.minify(file, {
   compress: {
     global_defs: {
       HTMX_JSON_DEBUG: false,
     },
   },
+  sourceMap: {
+    filename: "../src/htmx-json.js",
+    url: "./htmx-json.min.js.map",
+  },
 });
 await writeFile("./dist/htmx-json.min.js", code);
+await writeFile("./dist/htmx-json.min.js.map", map);
 await writeFile("./dist/htmx-json.mjs", `${file}\nexport default htmxJson;`);
 await writeFile("./dist/htmx-json.cjs", `${file}\module.exports htmxJson;`);
