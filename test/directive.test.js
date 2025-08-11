a0d32bc13cbddc5c9d422835af359de2340f7eba
Marius Gundersen <me@mariusgundersen.net>
2025-08-11 11:59:01 +0200

    feat: Added custom directives (#33)
    This fixes #1

---
+++ test/directive.test.js
@@ -0,0 +1,26 @@
+describe('directives', () => {
+  const classListDirective = {
+    match: (attr) => attr.name === '#class-list',
+    factory: (elm, attr, createGetter) => {
+      const getter = createGetter(attr.value);
+      return ($ctx) => {
+        const list = getter($ctx);
+        elm.classList.value = list.join(' ');
+      }
+    }
+  };
+  beforeAll(() => {
+    htmxJson.directives.push(classListDirective);
+  })
+  afterAll(() => {
+    htmxJson.directives.splice(htmxJson.directives.indexOf(classListDirective), 1);
+  })
+  it('should be able to add features', () => {
+    const div = document.createElement('div');
+    div.innerHTML = `<div #class-list="classes"></div>`;
+    htmxJson.swap(div, {
+      classes: ['test', 'something', 'here']
+    });
+    expect(div.innerHTML).toBe(`<div #class-list="classes" class="test something here"></div>`);
+  })
+})
