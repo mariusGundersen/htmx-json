51f0ca3fff18111154bbdd0c6d0ba5eff14b6809
Marius Gundersen <marius.gundersen@clave.no>
2025-08-20 10:55:55 +0200

    feat: Preparse templates so that we don't have to parse them for each iteration of the list

---
+++ test/preparse.test.js
@@ -0,0 +1,31 @@
+describe("preparse", () => {
+  let profiling = 0;
+  const profilingDirective = {
+    match: (attr) => attr.name === '#profile',
+    factory: (elm, attr, createGetter) => {
+      profiling++;
+      return ($ctx) => {
+        // do nothing
+      }
+    }
+  };
+  beforeAll(() => {
+    htmxJson.directives.push(profilingDirective);
+  })
+  afterAll(() => {
+    htmxJson.directives.splice(htmxJson.directives.indexOf(profilingDirective), 1);
+  })
+
+  let div = document.createElement("div");
+  beforeEach(() => {
+    div = document.createElement("div");
+    profiling = 0;
+  });
+
+  it("should only create the #profiling once", () => {
+    div.innerHTML = `<template json-each="list"><span #profile>\${$this} </span></template>`;
+    htmxJson.swap(div, { list: ['one', 'two', 'three'] });
+    expect(div.textContent).toBe(`one two three `);
+    expect(profiling).toBe(1)
+  });
+});
